26000bcc0f9880f7b09f31fd4ff2e537ce0c1ea5
“YolimaM” <yolimamch@gmail.com>
2023-04-19 14:57:01 -0500

    quitando import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Navbar from './Componentes/Navbar';
-
+import Navbar from "./Componentes/Navbar";
+import React, { Component } from "react";
 
 const App = () => {
-  return <>
-  <Navbar/>
-  </>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
