d6ecd302ef4205a814fe2aae20dea1e5496798b0
“YolimaM” <yolimamch@gmail.com>
2023-04-19 15:41:28 -0500

    borrando cambios

--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -1,14 +1,16 @@
 import NavbarStyle from "../css/Navbar.module.css";
-import {FaShoppingCart} from "react-icons/fa";
+
 const Navbar = () => {
   return (
-  <section className={NavbarStyle.titulo}>
-    <picture className={NavbarStyle.logo}>
-      <img src="/imgs/01 logo.png" alt="Logo tejidos Chakana"/>
-    </picture>
-    <FaShoppingCart/>
-  </section>
-  )
-}
+    <section className={NavbarStyle.titulo}>
+      <picture className={NavbarStyle.logo}>
+        <img src="/imgs/01 logo.png" alt="Logo tejidos Chakana" />
+      </picture>
+      <button className={NavbarStyle.iconcart}>
+        <FaShoppingCart />
+      </button>
+    </section>
+  );
+};
 
 export default Navbar;
