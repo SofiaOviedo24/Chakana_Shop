714d72915f6384568cda8ce4164907070971e0a4
“SofiaOviedo24” <sofiagarcia4200@gmail.com>
2023-04-19 14:54:08 -0500

    agregar icono de compra

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from './Componentes/Navbar';
 
-
 const App = () => {
   return <>
   <Navbar/>
+
+  
   </>;
+  
 };
 
 export default App;
--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -1,10 +1,12 @@
 import NavbarStyle from "../css/Navbar.module.css";
+import {FaShoppingCart} from "react-icons/fa";
 const Navbar = () => {
   return (
   <section className={NavbarStyle.titulo}>
     <picture className={NavbarStyle.logo}>
       <img src="/imgs/01 logo.png" alt="Logo tejidos Chakana"/>
     </picture>
+    <FaShoppingCart/>
   </section>
   )
 }
