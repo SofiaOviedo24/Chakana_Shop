b6e14f029f78ba4347b326a120b371fc38f8d514
“YolimaM” <yolimamch@gmail.com>
2023-04-19 15:55:48 -0500

    agregando icono

--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -1,12 +1,14 @@
 import NavbarStyle from "../css/Navbar.module.css";
-
+import { FaShoppingCart } from "react-icons/fa";
 const Navbar = () => {
   return (
     <section className={NavbarStyle.titulo}>
       <picture className={NavbarStyle.logo}>
         <img src="/imgs/01 logo.png" alt="Logo tejidos Chakana" />
       </picture>
-      <FaShoppingCart />
+      <span className="iconmenu">
+        <FaShoppingCart title="Cart icon" className={NavbarStyle.cart} />
+      </span>
     </section>
   );
 };
