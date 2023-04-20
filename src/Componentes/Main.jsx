b85400d39c91b5dc55c1d2597e246cf6e4a58144
“SofiaOviedo24” <sofiagarcia4200@gmail.com>
2023-04-20 12:07:55 -0500

    agregando grilla en Main.jsx y csss

--- src/Componentes/Main.jsx
+++ src/Componentes/Main.jsx
@@ -4,22 +4,39 @@ const Main = () => {
   return (
     <section className={MainStyle.galeria}>
       <article className={MainStyle.left}>
-        
+        <picture className={MainStyle.pic1}>
+          <img src="/imgs/06_P5.jpg" alt="Strap 06"/>
+        </picture>
+        <picture className={MainStyle.pic2}>
+          <img src="/imgs/07_P6.jpg" alt="Strap 07"/>
+        </picture>
+        <picture className={MainStyle.pic3}>
+          <img src="/imgs/08_P7.jpg" alt="Strap 08"/>
+        </picture>
+        <picture className={MainStyle.pic4}>
+          <img src="/imgs/09_P8.jpg" alt="Strap 09"/>
+        </picture>
+        <picture className={MainStyle.pic5}>
+          <img src="/imgs/10_P9.jpg" alt="Strap 10"/>
+        </picture>
+        <picture className={MainStyle.pic6}>
+          <img src="/imgs/11_P10.jpg" alt="Strap 11"/>
+        </picture>
       </article>
       <article className={MainStyle.right}>
-        <picture>
+        <picture className={MainStyle.pic7}>
           <img src="/imgs/03_P2.jpg" alt="Strap 01" />
         </picture>
 
-        <picture>
+        <picture className={MainStyle.pic8}>
           <img src="/imgs/04_P3.jpg" alt="Strap 02" />
         </picture>
 
-        <picture>
+        <picture className={MainStyle.pic9}>
           <img src="/imgs/05_P4.jpg" alt="Strap 03" />
         </picture>
 
-        <picture>
+        <picture className={MainStyle.pic10}>
           <img src="/imgs/02_P1.jpg" alt="tejido carlos pizarro" />
         </picture>
       </article>
