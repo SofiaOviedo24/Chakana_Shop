d71c15de09c309662149307745868605c40c29c2
“YolimaM” <yolimamch@gmail.com>
2023-04-20 09:58:19 -0500

    cambios en maincss y mainjsx

--- src/Componentes/Main.jsx
+++ src/Componentes/Main.jsx
@@ -1,45 +1,27 @@
 import MainStyle from "../css/Main.module.css";
 
 const Main = () => {
- return(
+  return (
+    <section className={MainStyle.galeria}>
+      <article className={MainStyle.right}>
+        <picture>
+          <img src="/imgs/03_P2.jpg" alt="Strap 01" />
+        </picture>
 
-<section className={MainStyle.galeria}>
+        <picture>
+          <img src="/imgs/04_P3.jpg" alt="Strap 02" />
+        </picture>
 
-  <picture className={MainStyle.p1}>
-   <img src="/imgs/02_P1.jpg" alt="tabloide carloz pizarro" />
-  </picture>
+        <picture>
+          <img src="/imgs/05_P4.jpg" alt="Strap 03" />
+        </picture>
 
-<article className={MainStyle.grid3}>
-
-  <picture className={MainStyle.p2}>
-   <img src="/imgs/03_P2.jpg" alt="strap 1" />
-  </picture>
-
-  <picture className={MainStyle.p3}>
-   <img src="/imgs/04_P3.jpg" alt="strap 2" />
-  </picture>
-
-  <picture className={MainStyle.p4}>
-   <img src="/imgs/05_P4.jpg" alt="strap 3" />
-  </picture>
-
-</article >
-
-<article className={MainStyle.grid1}>
-
-  <picture className={MainStyle.p5}>
-   <img src="/imgs/06_P5.jpg" alt="strap 4" />
-  </picture>
-
-  <picture className={MainStyle.p6}>
-   <img src="/imgs/07_P6.jpg" alt="model 1" />
-  </picture>
-
-</article>
-
-</section>
-
- )
-}
+        <picture>
+          <img src="/imgs/02_P1.jpg" alt="tejido carlos pizarro" />
+        </picture>
+      </article>
+    </section>
+  );
+};
 
 export default Main;
