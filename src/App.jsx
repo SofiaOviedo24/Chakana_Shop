8d663e4860a6ab930496201b448c3af88febf2e5
“Ivan-Useche-Rossi” <ifur94@gmail.com>
2023-04-21 15:17:15 -0500

    creacion catalgo (jsx y css)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Componentes/Navbar";
 import Main from "./Componentes/Main"
+import Catalogo from "./Componentes/Catalogo";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Main />
+      <Catalogo />
     </>
   );
 };
