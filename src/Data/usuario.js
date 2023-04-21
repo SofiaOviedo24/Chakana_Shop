93fb889be825df232068478bc77ed0a45b8531da
“SofiaOviedo24” <sofiagarcia4200@gmail.com>
2023-04-21 18:57:49 -0500

    Se hacen carpetas js de categorias, productos y usuarios

---
+++ src/Data/usuario.js
@@ -0,0 +1,4 @@
+let usuarioData = []
+usuarioData.push({username: "TejidosChakana", password:"TejidosChakana2023", roll:"Administrador"})
+usuarioData.push({username: "Mateo Perea", password:"Mateo2023", roll:"Cliente"});
+export default usuarioData;
