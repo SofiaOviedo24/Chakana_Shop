a8378ffdc629caebc273bc50f6b6c00cf6b19a81
“SofiaOviedo24” <sofiagarcia4200@gmail.com>
2023-04-20 21:43:52 -0500

    Correccion de Main.jsx para agregar boton

--- src/Componentes/Main.jsx
+++ src/Componentes/Main.jsx
@@ -2,49 +2,44 @@ import MainStyle from "../css/Main.module.css";
 
 const Main = () => {
   return (
-    <section className={MainStyle.galeria}>
-
-      <article className={MainStyle.left}>
-        <picture className={MainStyle.pic1}>
-          <img src="/imgs/06_P5.jpg" alt="Strap 06"/>
-        </picture>
-        <picture className={MainStyle.pic2}>
-          <img src="/imgs/07_P6.jpg" alt="Strap 07"/>
-        </picture>
-        <picture className={MainStyle.pic3}>
-          <img src="/imgs/08_P7.jpg" alt="Strap 08"/>
-        </picture>
-        <picture className={MainStyle.pic4}>
-          <img src="/imgs/09_P8.jpg" alt="Strap 09"/>
-        </picture>
-        <picture className={MainStyle.pic5}>
-          <img src="/imgs/10_P9.jpg" alt="Strap 10"/>
-        </picture>
-
-      </article>
-      <article className={MainStyle.right}>
-        <picture className={MainStyle.pic7}>
-          <img src="/imgs/03_P2.jpg" alt="Strap 01" />
-        </picture>
-        <picture className={MainStyle.pic8}>
-          <img src="/imgs/04_P3.jpg" alt="Strap 02" />
-        </picture>
-        <picture className={MainStyle.pic9}>
-          <img src="/imgs/05_P4.jpg" alt="Strap 03" />
-        </picture>
-        <picture className={MainStyle.pic10}>
-          <img src="/imgs/02_P1.jpg" alt="tejido carlos pizarro" />
-        </picture>
-      </article>
-
+    <section className={MainStyle.shop}>
+      <section className={MainStyle.galeria}>
+        <article className={MainStyle.left}>
+          <picture className={MainStyle.pic1}>
+            <img src="/imgs/06_P5.jpg" alt="Strap 06" />
+          </picture>
+          <picture className={MainStyle.pic2}>
+            <img src="/imgs/07_P6.jpg" alt="Strap 07" />
+          </picture>
+          <picture className={MainStyle.pic3}>
+            <img src="/imgs/08_P7.jpg" alt="Strap 08" />
+          </picture>
+          <picture className={MainStyle.pic4}>
+            <img src="/imgs/09_P8.jpg" alt="Strap 09" />
+          </picture>
+          <picture className={MainStyle.pic5}>
+            <img src="/imgs/10_P9.jpg" alt="Strap 10" />
+          </picture>
+        </article>
+        <article className={MainStyle.right}>
+          <picture className={MainStyle.pic7}>
+            <img src="/imgs/03_P2.jpg" alt="Strap 01" />
+          </picture>
+          <picture className={MainStyle.pic8}>
+            <img src="/imgs/04_P3.jpg" alt="Strap 02" />
+          </picture>
+          <picture className={MainStyle.pic9}>
+            <img src="/imgs/05_P4.jpg" alt="Strap 03" />
+          </picture>
+          <picture className={MainStyle.pic10}>
+            <img src="/imgs/02_P1.jpg" alt="tejido carlos pizarro" />
+          </picture>
+        </article>
+      </section>
       <span>
-      <button className={MainStyle.catalogo}>
-       C A T A L O G O
-      </button>
+        <button className={MainStyle.catalogo}>C A T A L O G O</button>
       </span>
-
     </section>
-
   );
 };
 
