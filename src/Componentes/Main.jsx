8e3203fbed380fb003bc4a618d83f0a8cb7ca5f9
“Ivan-Useche-Rossi” <ifur94@gmail.com>
2023-04-20 21:03:52 -0500

    Creacion (parcial) del boton "catalogo" junt con css

--- src/Componentes/Main.jsx
+++ src/Componentes/Main.jsx
@@ -3,6 +3,7 @@ import MainStyle from "../css/Main.module.css";
 const Main = () => {
   return (
     <section className={MainStyle.galeria}>
+
       <article className={MainStyle.left}>
         <picture className={MainStyle.pic1}>
           <img src="/imgs/06_P5.jpg" alt="Strap 06"/>
@@ -25,20 +26,25 @@ const Main = () => {
         <picture className={MainStyle.pic7}>
           <img src="/imgs/03_P2.jpg" alt="Strap 01" />
         </picture>
-
         <picture className={MainStyle.pic8}>
           <img src="/imgs/04_P3.jpg" alt="Strap 02" />
         </picture>
-
         <picture className={MainStyle.pic9}>
           <img src="/imgs/05_P4.jpg" alt="Strap 03" />
         </picture>
-
         <picture className={MainStyle.pic10}>
           <img src="/imgs/02_P1.jpg" alt="tejido carlos pizarro" />
         </picture>
       </article>
+
+      <span>
+      <button className={MainStyle.catalogo}>
+       C A T A L O G O
+      </button>
+      </span>
+
     </section>
+
   );
 };
 
