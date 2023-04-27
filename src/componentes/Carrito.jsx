c54a52eebfd069bd5351ca964d1ef51b5cd9c21b
Eduardo Miguel Virgilio <126211637+EduardoMVBit@users.noreply.github.com>
2023-04-27 21:00:29 +0000

    Actualizando Estilos del Carrito

--- src/componentes/Carrito.jsx
+++ src/componentes/Carrito.jsx
@@ -1,13 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext,useState} from "react";
 import { FaShoppingCart, FaTimes } from "react-icons/fa";
 import CartContext from "../context/Cart";
 import CarritoStyle from "../css/Carrito.module.css"
 const Carrito = function () {
+  const [show, setShow] = useState(false)
   const { cart, actualizar, eliminar } = useContext(CartContext);
   return (
     <>
-      <FaShoppingCart className={CarritoStyle.icon} />
-      <section className={CarritoStyle.items}>
+      <FaShoppingCart className={CarritoStyle.icon} onClick={() => setShow(!show)} />
+      <section className={`${CarritoStyle.items} ${show ? CarritoStyle.active : '' }`}>
         {cart && cart.items.length > 0 ? (
           <ul>
             {cart.items.map(function (item) {
