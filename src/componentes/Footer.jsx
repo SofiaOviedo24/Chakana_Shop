d749c480a778e44ac921d33d956526118e188de3
“Ivan-Useche-Rossi” <ifur94@gmail.com>
2023-05-02 15:30:24 -0500

    Creacion footer (jsx y css)

---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,42 @@
+import FooterStyle from "../css/Footer.module.css";
+import { FaUserAlt } from "react-icons/fa"
+
+const Footer = () => {
+ return (
+
+  
+<section className={FooterStyle.ctc}>
+
+  <h2 className={FooterStyle.contact}>!Trabajemos Juntos!</h2>
+      <form action="#" method="post" className={FooterStyle.newsletter}>
+        <input type="E-mail" placeholder="E-mail" id="E-mail" name="E-mail" />
+        <button type="submit">
+        <FaUserAlt title="Instagram icon"  />
+        </button>
+        <input
+          type="Mensaje"
+          placeholder="Mensaje"
+          id="Mensaje"
+          name="Mensaje"
+          />
+        <button type="submit"></button>
+      </form>
+
+
+
+      <article className={FooterStyle.contact2}>
+        <h3>Tejidos Chakana</h3>
+        <h4>Bogotá DC. - Colombia</h4>
+        <nav>
+          <a href="https://www.instagram.com/tejidoschakana/" target="blank">
+          <FaUserAlt title="Instagram icon" className={FooterStyle.ig} />
+          </a>
+        </nav>
+      </article>
+
+</section>
+
+ );
+};
+
+export default Footer;
