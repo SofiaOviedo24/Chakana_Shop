bdcd8e6df557ee41a0b1e68a9afbc00168b9d59f
“YolimaM” <yolimamch@gmail.com>
2023-05-03 16:25:30 -0500

    Cambios varios

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
-<<<<<<< HEAD
 import Navbar from "./componentes/Navbar";
-import Main from "./componentes/Main"
+import Main from "./componentes/Main";
 import Productos from "./componentes/Productos";
 import Detalle from "./componentes/Detalle";
 import Footer from "./componentes/Footer";
-=======
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import ForLogin from './componentes/FormLogin';
-
->>>>>>> melendez
+import ForLogin from "./componentes/FormLogin";
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
       <Navbar />
       <Main />
-      <Productos/>
-      <Detalle/>
-      <Footer/>
-=======
-      
-      <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<ForLogin />}/>
-      </Routes>
-      </BrowserRouter>
->>>>>>> melendez
+      <Productos />
+      <Detalle />
+      <ForLogin />
+      <Footer />
     </>
   );
 };
--- src/componentes/Detalle.jsx
+++ src/componentes/Detalle.jsx
@@ -2,53 +2,53 @@ import React, { useContext, useState } from "react";
 import DetalleStyle from "../css/Detalle.module.css";
 import ProductoContext from "../context/Producto";
 import CartContext from "../context/Cart";
-import { FaShoppingCart} from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 import { RxCross1 } from "react-icons/rx";
 
 const Detalle = function () {
   const { producto, setProducto } = useContext(ProductoContext);
-  const { agregar} = useContext(CartContext);
+  const { agregar } = useContext(CartContext);
   return (
     <>
       {producto == null ? null : (
-<<<<<<< HEAD
         <section className={DetalleStyle.inicio}>
-        <span className={DetalleStyle.contenedor}>
-          <form className={DetalleStyle.cerrar}
-=======
-        <section id="Detalle" className={DetalleStyle.inicio}>
-        <article className={DetalleStyle.contenedor}>
-          <form 
->>>>>>> melendez
-            onSubmit={function () {
-              setProducto(null);
-            }}
-          >
-            <button>
-              <RxCross1  title="Cerrar" className={DetalleStyle.x}/>
-            </button>
-          </form>
-          <picture className={DetalleStyle.item}>
-            <img src={producto.imagen} alt="" />
-          </picture>
-          <article className={DetalleStyle.info}>
-          <h1>
-            {producto.name}  
-          </h1>
-          <h2> Descripción: </h2>
-          <p>{producto.descripcion}</p>
-          <h3> Precio </h3>
-          <h4> ${producto.precio} </h4>
+          <article className={DetalleStyle.contenedor}>
+            <form
+              className={DetalleStyle.cerrar}
+              onSubmit={function () {
+                setProducto(null);
+              }}
+            >
+              <button>
+                <RxCross1 title="Cerrar" className={DetalleStyle.x} />
+              </button>
+            </form>
+            <picture className={DetalleStyle.item}>
+              <img src={producto.imagen} alt="" />
+            </picture>
+            <article className={DetalleStyle.info}>
+              <h1>{producto.name}</h1>
+              <h2> Descripción: </h2>
+              <p>{producto.descripcion}</p>
+              <h3> Precio </h3>
+              <h4> ${producto.precio} </h4>
+            </article>
           </article>
-        </span>
-          
-          <form className={DetalleStyle.boton}
-            onSubmit={function(event) {
-              event.preventDefault()
+
+          <form
+            className={DetalleStyle.boton}
+            onSubmit={function (event) {
+              event.preventDefault();
               agregar(producto.id);
             }}
           >
-            <button> AGREGAR <span><FaShoppingCart title="carrito" /></span></button>
+            <button>
+              {" "}
+              AGREGAR{" "}
+              <span>
+                <FaShoppingCart title="carrito" />
+              </span>
+            </button>
           </form>
         </section>
       )}
