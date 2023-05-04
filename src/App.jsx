7b39ca3cfa4172d0493fcfd8b3111115674efd62
“Ivan-Useche-Rossi” <ifur94@gmail.com>
2023-05-04 10:28:26 -0500

    Actualizacion version 03-05-2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,19 @@
-<<<<<<< HEAD
-
-
-
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import ForLogin from './componentes/FormLogin';
-
-
-=======
 import Navbar from "./componentes/Navbar";
 import Main from "./componentes/Main";
 import Productos from "./componentes/Productos";
 import Detalle from "./componentes/Detalle";
 import Footer from "./componentes/Footer";
 import ForLogin from "./componentes/FormLogin";
->>>>>>> 7244ab8d2691611cb9bef7a2b58b98a0b2ea5992
 
 const App = () => {
   return (
     <>
-<<<<<<< HEAD
-
-      
-
-      
-      <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<ForLogin />}/>
-      </Routes>
-      </BrowserRouter>
-
-=======
       <Navbar />
       <Main />
       <Productos />
       <Detalle />
       <ForLogin />
       <Footer />
->>>>>>> 7244ab8d2691611cb9bef7a2b58b98a0b2ea5992
     </>
   );
 };
--- src/componentes/Detalle.jsx
+++ src/componentes/Detalle.jsx
@@ -11,58 +11,26 @@ const Detalle = function () {
   return (
     <>
       {producto == null ? null : (
-<<<<<<< HEAD
 
-        <section className={DetalleStyle.inicio}>
-        <span className={DetalleStyle.contenedor}>
-          <form className={DetalleStyle.cerrar}
 
-        <section id="Detalle" className={DetalleStyle.inicio}>
-          <article className={DetalleStyle.contenedor}>
-            <form 
->>>>>>> melendez
-=======
+
         <section className={DetalleStyle.inicio}>
           <article className={DetalleStyle.contenedor}>
             <form
               className={DetalleStyle.cerrar}
->>>>>>> 7244ab8d2691611cb9bef7a2b58b98a0b2ea5992
+
               onSubmit={function () {
                 setProducto(null);
               }}
             >
               <button>
-<<<<<<< HEAD
-                <RxCross1  title="Cerrar" className={DetalleStyle.x}/>
-=======
                 <RxCross1 title="Cerrar" className={DetalleStyle.x} />
->>>>>>> 7244ab8d2691611cb9bef7a2b58b98a0b2ea5992
               </button>
             </form>
             <picture className={DetalleStyle.item}>
               <img src={producto.imagen} alt="" />
             </picture>
             <article className={DetalleStyle.info}>
-<<<<<<< HEAD
-            <h1>
-              {producto.name}  
-            </h1>
-            <h2> Descripción: </h2>
-            <p>{producto.descripcion}</p>
-            <h3> Precio </h3>
-            <h4> ${producto.precio} </h4>
-            </article>
-          </span>
-            
-            <form className={DetalleStyle.boton}
-              onSubmit={function(event) {
-                event.preventDefault()
-                agregar(producto.id);
-              }}
-            >
-              <button> AGREGAR <span><FaShoppingCart title="carrito" /></span></button>
-            </form>
-=======
               <h1>{producto.name}</h1>
               <h2> Descripción: </h2>
               <p>{producto.descripcion}</p>
@@ -86,7 +54,6 @@ const Detalle = function () {
               </span>
             </button>
           </form>
->>>>>>> 7244ab8d2691611cb9bef7a2b58b98a0b2ea5992
         </section>
       )}
     </>
